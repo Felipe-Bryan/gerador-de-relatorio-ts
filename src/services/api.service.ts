2c00eb55ab284dfaf72a0ad4701838060f62719d
Felipe Bryan <felipe.bryan@live.com>
2023-09-13 21:08:20 -0300

    getApiData started

---
+++ src/services/api.service.ts
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import { CreateVendaParams } from '../types/CreateVendaParams';
+
+const api = axios.create({
+  baseURL: 'http://localhost:3333/vendas',
+});
+
+export const createVendaApi = (params: CreateVendaParams) => {
+  api.post('/', params).then((res) => {
+    console.log(res.data.data);
+  });
+};
